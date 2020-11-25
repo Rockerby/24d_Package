132c472f0e91e48d8e1c38e8cdc3004f8f8d1f03
Richard Ockerby <richard.ockerby@drummondcentral.co.uk>
2020-11-25 13:00:15 +0000

    Core plugin setup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# 24d_Package
+# Umbraco : More Info App
+
+This is a sample content app which has a simple preview frame along with some data about the content that is currently being edited.
+
+It's used as reference for the 24 days in Umbraco article 'Make your custom back-end lovely'.
---
+++ src/24days/App_Plugins/24days/MoreInfoApp/moreinfoapp.controller.js
@@ -0,0 +1,52 @@
+﻿/**
+ * @ngdoc
+ * 
+ * @name: dashboardController
+ * @description: code for a dashboard in the umbraco back office
+ * 
+ */
+(function () {
+    'use strict';
+
+    function moreInfoAppController($scope,
+        editorState, userService, contentResource) {
+
+        var vm = this;
+        vm.CurrentNodeId = editorState.current.id;
+        vm.CurrentNodeAlias = editorState.current.contentTypeAlias;
+        vm.loading = true;
+        vm.iframeSrc = '/umbraco/preview/?id=' + vm.CurrentNodeId;
+        vm.userName = '';
+        vm.contentNode = {};
+        vm.numberOfProperties = 0;
+
+
+        contentResource.getById(vm.CurrentNodeId).then(function (node) {
+            vm.contentNode = node;
+            console.log('node', node);
+            
+            node.variants[0].tabs.forEach((tab) => {
+                vm.numberOfProperties += tab.properties.length;    
+            });
+            vm.loading = false;
+            //var properties = node.variants[0].tabs[0].properties;
+
+            //vm.propertyWordCount = {};
+
+            //var index;
+            //for (index = 0; index < properties.length; ++index) {
+            //    var words = properties[index].value;
+            //    var wordCount = words.trim().split(/\s+/).length;
+
+            //    vm.propertyWordCount[properties[index].label] = wordCount;
+            //}
+        });
+
+        userService.getCurrentUser().then(function (user) {
+            vm.userName = user.name;
+        });
+    }
+
+    angular.module('umbraco')
+        .controller('moreInfoAppController', moreInfoAppController);
+})();
